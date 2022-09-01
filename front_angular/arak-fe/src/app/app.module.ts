513d81fe5dd625e69745aba427cf5f5c74ca3dab
andylopezgit <alopezciotta@gmail.com>
2022-09-01 15:29:25 -0300

    se agregam variables usuario y pass para login, mas spinner de material, tambien logica para error de datos ingresados

--- front_angular/arak-fe/src/app/app.module.ts
+++ front_angular/arak-fe/src/app/app.module.ts
@@ -4,12 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 /* Material angular modules */
 import { MatSliderModule } from '@angular/material/slider';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 /* Componentes */
 import { LoginComponent } from './components/login/login.component';
+
 
 @NgModule({
   declarations: [
@@ -21,6 +28,12 @@ import { LoginComponent } from './components/login/login.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front_angular/arak-fe/src/app/components/login/login.component.ts
+++ front_angular/arak-fe/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
+  loading: boolean;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private _snackBar: MatSnackBar) {
+    this.form = this.fb.group({
+      usuario: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+    this.loading = false
+  }
 
   ngOnInit(): void {
+  }
+
+  ingresar() {
+    console.log(this.form)
+    const usuario = this.form.value.usuario;
+    const password = this.form.value.password;
+
+    if (usuario === 'andy' && password === '1234') {
+      //ingreso al dashboard
+      this.loading = true;
+    } else {
+      // mensaje de error
+      this.error();
+    }
+
+  }
+
+  error() {
+    this._snackBar.open('Usuario o contraseña invalidos', '', {
+      duration: 5000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom'
+    })
   }
 
 }
