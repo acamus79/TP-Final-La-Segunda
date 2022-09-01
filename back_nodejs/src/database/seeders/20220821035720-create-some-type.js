b2617fc47b399b581d60aa3236dd64fed3b0a0d5
andylopezgit <alopezciotta@gmail.com>
2022-09-01 08:10:10 -0300

    se completan seeders

--- back_nodejs/src/database/seeders/20220821035720-create-some-type.js
+++ back_nodejs/src/database/seeders/20220821035720-create-some-type.js
@@ -1,24 +1,37 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('types', [
+      {
+        description: 'Auto',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        description: 'Moto',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        description: 'Camion',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        description: 'Camioneta',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        description: 'Taxi/Remis',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    ], {});
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('types', null, {});
   }
 };
