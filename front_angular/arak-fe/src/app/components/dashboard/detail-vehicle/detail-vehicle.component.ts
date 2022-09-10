fbaa84cdc0ab44397f975306ef67294c8e679510
andylopezgit <alopezciotta@gmail.com>
2022-09-10 09:55:26 -0300

    se agrega imagen de usuario y estetica del home

--- front_angular/arak-fe/src/app/components/dashboard/detail-vehicle/detail-vehicle.component.ts
+++ front_angular/arak-fe/src/app/components/dashboard/detail-vehicle/detail-vehicle.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-detail-vehicle',
   templateUrl: './detail-vehicle.component.html',
-  styleUrls: ['./detail-vehicle.component.css']
+  styleUrls: ['./detail-vehicle.component.css'],
 })
 export class DetailVehicleComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- front_angular/arak-fe/src/app/components/dashboard/users/users.component.ts
+++ front_angular/arak-fe/src/app/components/dashboard/users/users.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, ViewChild, ViewChildren, AfterViewInit, OnDestroy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ViewChildren,
+  AfterViewInit,
+  OnDestroy,
+} from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { IReqUser, ReqUser } from 'src/app/models/iusuario';
 import { UserService } from 'src/app/services/user/user.service';
@@ -6,68 +13,46 @@ import { DialogAddUserComponent } from '../../shared/dialog-add-user/dialog-add-
 import { MatDialog } from '@angular/material/dialog';
 import { Subscription } from 'rxjs';
 
-
-
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css'],
-  template: `message: <app-dialog-add-user></app-dialog-add-user>`
+  template: `message: <app-dialog-add-user></app-dialog-add-user>`,
 })
-
 export class UsersComponent implements OnInit, OnDestroy {
   m: number = 0;
-  suscription?:Subscription;
-  message: string = ''
-  @ViewChild(DialogAddUserComponent) child:any;
+  suscription?: Subscription;
+  message: string = '';
+  @ViewChild(DialogAddUserComponent) child: any;
 
-  colum = [
-    "id",
-    "name",
-    "email",
-    "role",
-    "actions"
-  ];
+  colum = ['id', 'name', 'email', 'role', 'actions'];
 
-  
   displayedColumns: string[] = this.colum;
-  dataSource: MatTableDataSource<ReqUser> = new MatTableDataSource([] as ReqUser[])
+  dataSource: MatTableDataSource<ReqUser> = new MatTableDataSource(
+    [] as ReqUser[]
+  );
 
-  constructor(
-    private userService: UserService,
-    public dialog: MatDialog,
-
-
-  ) { }
+  constructor(private userService: UserService, public dialog: MatDialog) {}
 
   ngOnInit(): void {
-
-    this.getUsers();  
+    this.getUsers();
 
     this.suscription = this.userService.refresh$.subscribe(() => {
       this.getUsers();
-    })
-
+    });
   }
 
   getUsers(): void {
-    this.userService.getUser$()
-      .subscribe((res: any) => {
-        console.log(res)
-        this.dataSource.data = res.body.data;
-        console.log('esto se eejecuta otra vez');      
-      })
+    this.userService.getUser$().subscribe((res: any) => {
+      console.log(res);
+      this.dataSource.data = res.body.data;
+      console.log('esto se eejecuta otra vez');
+    });
   }
 
   openDialogAddUser() {
-    this.dialog.open(DialogAddUserComponent)
-
+    this.dialog.open(DialogAddUserComponent);
   }
 
-  ngOnDestroy(): void {
-    
-  }
-
-
-
+  ngOnDestroy(): void {}
 }
