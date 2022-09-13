b05be217040da4fed97894bf8d020c5b1d98b673
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2022-09-13 17:05:34 -0300

    Update server.js

--- back_nodejs/src/server.js
+++ back_nodejs/src/server.js
@@ -24,12 +24,18 @@ const routerContact = require('./routes/contact.routes');
 //Settings
 app.use(express.json());
 app.use(express.urlencoded({
-  extend: false
+  extend: true
 }));
 app.use(json());
-app.use(cors({
-  origin: '*'
-}));
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("preflightContinue", false);
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next()
+});
 
 //Rutas
 
